fefb67fe4f1c219b38242b29f990d7853236e080
pmy <meiyinpu@gmail.com>
2024-05-21 00:23:53 +0200

    install UI libraries : flowbite and react-icons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,16 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Dashboard from './pages/Dashboard'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
+import SignIn from './pages/Signin'
+import SignUp from './pages/Signup'
 import Projects from './pages/Projects'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <>
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,27 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//     "node_modules/flowbite-react/**/*.{js, jsx, ts, tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [require('flowbite/plugin')],
+// }
+
+
+import flowbite from "flowbite-react/tailwind";
+
 /** @type {import('tailwindcss').Config} */
-export default {
+export default  {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+  plugins: [
+    flowbite.plugin(),
+  ],
+};

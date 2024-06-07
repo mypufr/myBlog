92f1c76279be218e492c7687cc24ce6fab51e42d
MeiyinPU <mei-yin.pu@oclock.school>
2024-06-07 17:19:43 +0200

    install react-icons

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,17 +1,3 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,ts,jsx,tsx}",
-//     "node_modules/flowbite-react/**/*.{js, jsx, ts, tsx}",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [require('flowbite/plugin')],
-// }
-
-
 import flowbite from "flowbite-react/tailwind";
 
 /** @type {import('tailwindcss').Config} */

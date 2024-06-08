c3f5c10a76356475675b039c98884b318b7a0747
MeiyinPU <mei-yin.pu@oclock.school>
2024-06-08 07:18:13 +0200

    Header with hamburger menu

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,46 +1,61 @@
-import { Navbar, TextInput, Button } from "flowbite-react"
-import { Link } from "react-router-dom"
+import { Navbar, TextInput, Button} from "flowbite-react"
+import { Link, useLocation } from "react-router-dom"
 import { AiOutlineSearch} from "react-icons/ai"
 import { FaMoon} from "react-icons/fa"
 
 export default function Header() {
+    const path = useLocation().pathname; 
+
     return (
         <Navbar className="border-b-2">
             <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
                 <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white ">MY</span>Blog
             </Link>
-<form>
-<TextInput 
-type="text"
-placeholder="Search..."
-rightIcon={AiOutlineSearch}
-className="hidden lg:inline"
-/>
-</form>
 
-<Button className="w-12 h-10 lg:hidden" color="gray" pill>
- <AiOutlineSearch />
-</Button>
+            <form>
+            <TextInput 
+            type="text"
+            placeholder="Search..."
+            rightIcon={AiOutlineSearch}
+            className="hidden lg:inline"
+            />
+            </form>
 
-<div className="flex gap-2 md:order-2">
-    {/* set the order of the 2 elements in middle size screen(768px) */}
+            <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+            <AiOutlineSearch />
+            </Button>
 
-    <Button className="w-12 h-10 
-    // hidden 
-    sm:inline
-    " color="gray" pill>
-        <FaMoon />
-    </Button>
 
-    <Link to="sign-in">
-    
-    <Button gradientDuoTone="purpleToBlue">
-        Sign In
-    </Button>
-    
-    </Link>
-</div>
+            <div className="flex gap-2 md:order-1">
+            {/* set the order of the 2 elements in middle size screen(768px) md:order-2 类在中等及以上屏幕尺寸（768px 及以上）时，将这个 div 排列在父容器中的第二位。这意味着在这些屏幕尺寸下，这个 div 将排在其他未设置 order 值的元素之后*/}
 
+            <Button className="w-12 h-10 
+            hidden 
+            sm:inline
+            " color="gray" pill>
+                <FaMoon />
+            </Button>
+
+            <Link to="sign-in">
+
+            <Button gradientDuoTone="purpleToBlue">
+                Sign In
+            </Button>
+            </Link>
+            <Navbar.Toggle />
+            </div>
+
+            <Navbar.Collapse>
+            <Navbar.Link active={path === "/"} as={"div"}>
+                <Link to="/">Home</Link>
+            </Navbar.Link>
+            <Navbar.Link active={path === "/about"} as={"div"}>
+                <Link to="/about">About</Link>
+            </Navbar.Link>
+            <Navbar.Link active={path === "/projects"} as={"div"}>
+                <Link to="/projects">Projects</Link>
+            </Navbar.Link>
+            </Navbar.Collapse>
         </Navbar>
     )
 }

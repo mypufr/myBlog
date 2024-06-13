1ebd6ea488a8d7727d9c590b97036989c89532da
MeiyinPU <mei-yin.pu@oclock.school>
2024-06-14 00:08:08 +0200

    create and add the footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,86 @@
+import { Footer } from "flowbite-react";
+import { Link } from "react-router-dom";
+import {
+  BsFacebook,
+  BsInstagram,
+  BsTwitter,
+  BsGithub,
+  BsDribbble,
+} from "react-icons/bs";
+
+export default function FooterCom() {
+  return (
+    <Footer container className="border border-t-8 border-teal-500">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className="mt-5">
+            <Link
+              to="/"
+              className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
+            >
+              <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white ">
+                MY
+              </span>
+              Blog
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="About" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="https://google.com" target="_blank" rel="">
+                  100 JS Projects
+                </Footer.Link>
+                <Footer.Link href="https://google.com" target="_blank" rel="">
+                  MyBlog
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Follow us" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://github.com/mypufr"
+                  target="_blank"
+                  rel=""
+                >
+                  Github
+                </Footer.Link>
+                <Footer.Link href="#" target="_blank" rel="">
+                  Discord
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal Notices" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#" target="_blank" rel="">
+                  Privact Policy
+                </Footer.Link>
+                <Footer.Link href="#" target="_blank" rel="">
+                  Terms & Conditions
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright
+            href="#"
+            by="MY Blog"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon href="#" icon={BsFacebook} />
+            <Footer.Icon href="#" icon={BsInstagram} />
+            <Footer.Icon href="#" icon={BsTwitter} />
+            <Footer.Icon href="http://github.com/mypufr" icon={BsGithub} />
+            <Footer.Icon href="#" icon={BsDribbble} />
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
